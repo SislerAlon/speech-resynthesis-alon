ed4a604abe848e903d8931f109ec0a7ba1049054
Alon <alonsisler@gmail.com>
2022-06-18 13:19:06 +0300

    adding anonymos options

--- dash_app.py
+++ dash_app.py
@@ -4,7 +4,6 @@ from dash import Input, Output, html, dcc
 
 import inference_alon
 
-
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
@@ -17,15 +16,13 @@ speakers_data_path = r"C:\git\AccentTransfer\datasets\VCTK_092\speakers_data.csv
 speakers_data = pd.read_csv(speakers_data_path).set_index('Id')
 speakers_data_dict = speakers_data.to_dict('index')
 speakers_list = list(speakers_data_dict.keys())
-accent_list = speakers_list.copy() #speakers_data.Accent.unique().tolist()
-# Selecciona el color del header
+accent_list = speakers_list.copy()  # speakers_data.Accent.unique().tolist()
+
 COLOR_HEADER = "rgb(14, 88, 20)"
 
-
-data_sound_file = r"D:\Thesis\generated_results\generated_files_accent_change_double_zero_accent\p226_131_mic2_speaker_p248_accent_p248_gen.wav"#"data/Dog Snarling.mp3"
+data_sound_file = r"D:\Thesis\generated_results\generated_files_accent_change_double_zero_accent\p226_131_mic2_speaker_p248_accent_p248_gen.wav"  # "data/Dog Snarling.mp3"
 # data sound preparations
 data_sound = base64.b64encode(open(data_sound_file, 'rb').read())
-
 
 GENERATOR = inference_alon.init_generator('C:/git/speech-resynthesis-alon/tmp')
 speaker_to_dataset_index = GENERATOR.get_index_by_speaker()
@@ -33,6 +30,12 @@ speaker_to_dataset_index = GENERATOR.get_index_by_speaker()
 # Lista con los títulos y los filtros de la barra de la izquierda.
 MENU = [
     html.P("Base Speaker", className="title-filter"),
+    dcc.RadioItems(
+        id="anonymous_button",
+        options=[{"label": 'Non-Anonymous', "value": False}, {"label": 'Anonymous', "value": True}],
+        value=False,
+        inline=True
+    ),
     dcc.Dropdown(
         id="base_speaker_selector",
         options=[{"label": r, "value": r} for r in speakers_list],
@@ -91,27 +94,27 @@ app.layout = html.Div(
                 #     id="graphs",
                 #     children=GRAFICOS,
                 # ),
-            html.Audio(title='gt',
-                       id='gt_player',
-                       src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
-                       controls=True,
-                       autoPlay=False,
-                       style={"width": "100%"}
-               ),
-            html.Audio(title='gt_gen',
-                        id='gt_gen_player',
-                        src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
-                        controls=True,
-                        autoPlay=False,
-                        style={"width": "100%"}
-                ),
-            html.Audio(title='gt_gen',
-                        id='new_player',
-                        src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
-                        controls=True,
-                        autoPlay=False,
-                        style={"width": "100%"}
-                ),
+                html.Audio(title='gt',
+                           id='gt_player',
+                           src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
+                           controls=True,
+                           autoPlay=False,
+                           style={"width": "100%"}
+                           ),
+                html.Audio(title='gt_gen',
+                           id='gt_gen_player',
+                           src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
+                           controls=True,
+                           autoPlay=False,
+                           style={"width": "100%"}
+                           ),
+                html.Audio(title='gt_gen',
+                           id='new_player',
+                           src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
+                           controls=True,
+                           autoPlay=False,
+                           style={"width": "100%"}
+                           ),
             ],
             id="content",
         ),
@@ -119,29 +122,36 @@ app.layout = html.Div(
     id="wrapper",
 )
 
+
 def get_src_from_sound_file_path(data_sound_file):
     data_sound = base64.b64encode(open(data_sound_file, 'rb').read())
     return 'data:audio/mpeg;base64,{}'.format(data_sound.decode())
 
 
 @app.callback(
-    [Output("gt_player", "src"), Output("gt_gen_player", "src"), Output("new_player", "src"), Output("base_speaker_index_selector", "options")],
     [
+        Output("gt_player", "src"),
+        Output("gt_gen_player", "src"),
+        Output("new_player", "src"),
+        Output("base_speaker_index_selector", "options"),
+        Output("base_speaker_selector", "options"),
+        Output("target_speaker_selector", "options"),
+        Output("target_accent_selector", "options")
+    ],
+    [
+        Input("anonymous_button", "value"),
         Input("base_speaker_selector", "value"),
         Input("target_speaker_selector", "value"),
         Input("target_accent_selector", "value"),
         Input("base_speaker_index_selector", "value"),
     ],
 )
-def update_graphs(base_speaker, target_speaker, target_accent, base_speaker_index):
+def update_graphs(keep_anonymous, base_speaker, target_speaker, target_accent, base_speaker_index):
     sound_file_output_path = inference_alon.generate_from_speakers(generator=GENERATOR,
-                                        base_speaker=base_speaker,
-                                        target_speaker=target_speaker,
-                                        target_accent=target_accent,
-                                        base_speaker_index=base_speaker_index)
-
-
-
+                                                                   base_speaker=base_speaker,
+                                                                   target_speaker=target_speaker,
+                                                                   target_accent=target_accent,
+                                                                   base_speaker_index=base_speaker_index)
 
     print(sound_file_output_path)
 
@@ -151,7 +161,12 @@ def update_graphs(base_speaker, target_speaker, target_accent, base_speaker_inde
 
     number_of_utterance = len(speaker_to_dataset_index[base_speaker])
     utterance_options = [{"label": str(r), "value": r} for r in range(number_of_utterance)]
-    return gt_src, gt_gen_src, new_src, utterance_options
+    if keep_anonymous:
+        speaker_selector = [{"label": r, "value": r} for r in speakers_list]
+    else:
+        speaker_selector = [{"label": f'{r}_{speakers_data_dict[r]}', "value": r} for r in speakers_list]
+
+    return gt_src, gt_gen_src, new_src, utterance_options, speaker_selector, speaker_selector, speaker_selector
 
 
 if __name__ == "__main__":
