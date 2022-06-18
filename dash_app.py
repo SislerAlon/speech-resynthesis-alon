c5c8f32d3acbaca7c238c4e5ab1cdf0212a583e7
Alon <alonsisler@gmail.com>
2022-06-19 00:21:45 +0300

    adding accent and speaker classification

--- dash_app.py
+++ dash_app.py
@@ -1,22 +1,28 @@
 import dash
+import numpy as np
 
 from dash import Input, Output, html, dcc
 
 import inference_alon
+import accent_model
+import speaker_model
+import librosa
+import torch
 
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 from datetime import datetime
 import base64
+import resampy
 
 pio.templates.default = "simple_white"
 
-speakers_data_path = r"C:\git\AccentTransfer\datasets\VCTK_092\speakers_data.csv"
+speakers_data_path = r"src/speakers_data.csv"
 speakers_data = pd.read_csv(speakers_data_path).set_index('Id')
 speakers_data_dict = speakers_data.to_dict('index')
-speakers_list = list(speakers_data_dict.keys())
-accent_list = speakers_list.copy()  # speakers_data.Accent.unique().tolist()
+speakers_list = []#list(speakers_data_dict.keys())
+# accent_list = speakers_list.copy()  # speakers_data.Accent.unique().tolist()
 
 COLOR_HEADER = "rgb(14, 88, 20)"
 
@@ -25,17 +31,27 @@ data_sound_file = r"D:\Thesis\generated_results\generated_files_accent_change_do
 data_sound = base64.b64encode(open(data_sound_file, 'rb').read())
 
 GENERATOR = inference_alon.init_generator('C:/git/speech-resynthesis-alon/tmp')
+speakers_list = GENERATOR.dataset.id_to_spkr
 speaker_to_dataset_index = GENERATOR.get_index_by_speaker()
+
+accent_list = speakers_list.copy()
+
+accent_m = accent_model.AccentModel()
+accent_mapping_dict = accent_m.get_accent_mapping()
+
+speaker_m = speaker_model.SpeakerModel()
+
 
 # Lista con los títulos y los filtros de la barra de la izquierda.
 MENU = [
-    html.P("Base Speaker", className="title-filter"),
+    html.P("Anonymous", className="title-filter"),
     dcc.RadioItems(
         id="anonymous_button",
         options=[{"label": 'Non-Anonymous', "value": False}, {"label": 'Anonymous', "value": True}],
         value=False,
         inline=True
     ),
+    html.P("Base Speaker", className="title-filter"),
     dcc.Dropdown(
         id="base_speaker_selector",
         options=[{"label": r, "value": r} for r in speakers_list],
@@ -71,7 +87,6 @@ MENU = [
         clearable=False,
         style={"width": "100%"}
     ),
-
 ]
 
 # GRAFICOS = [dcc.Graph(id="line-chart"), dcc.Graph(id="donut-chart")]
@@ -101,6 +116,8 @@ app.layout = html.Div(
                            autoPlay=False,
                            style={"width": "100%"}
                            ),
+                html.Div(id='Base_Accent_results'),
+                html.Div(id='Base_speaker_results'),
                 html.Audio(title='gt_gen',
                            id='gt_gen_player',
                            src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
@@ -108,13 +125,17 @@ app.layout = html.Div(
                            autoPlay=False,
                            style={"width": "100%"}
                            ),
-                html.Audio(title='gt_gen',
+                html.Div(id='gt_gen_Accent_results'),
+                html.Div(id='gt_gen_speaker_results'),
+                html.Audio(title='new_gen',
                            id='new_player',
                            src='data:audio/mpeg;base64,{}'.format(data_sound.decode()),
                            controls=True,
                            autoPlay=False,
                            style={"width": "100%"}
                            ),
+                html.Div(id='new_gen_Accent_results'),
+                html.Div(id='new_gen_speaker_results'),
             ],
             id="content",
         ),
@@ -128,6 +149,34 @@ def get_src_from_sound_file_path(data_sound_file):
     return 'data:audio/mpeg;base64,{}'.format(data_sound.decode())
 
 
+# @app.callback(
+#     [
+#         Output("base_speaker_selector", "value"),
+#         Output("target_speaker_selector", "value"),
+#         Output("target_accent_selector", "value")
+#     ],
+#     [dash.dependencies.Input('random_button', 'n_clicks')],
+# )
+# def random_speakers():
+#     return 'p225'
+#     )
+
+def get_accent_classification(waveform_path):
+    # reading output waveforms
+    waveform, Librosa_sample_rate = librosa.load(waveform_path)
+    waveform = resampy.resample(waveform, Librosa_sample_rate, 16_000)
+    output_accent = accent_mapping_dict[np.argmax(accent_m(torch.Tensor(np.expand_dims(waveform, axis=0))))]
+    return output_accent
+
+def get_speaker_classification(waveform_path):
+    # reading output waveforms
+    waveform, Librosa_sample_rate = librosa.load(waveform_path)
+    waveform = resampy.resample(waveform, Librosa_sample_rate, 16_000)
+    # print(f'speaker_index :{np.argmax(speaker_m(torch.Tensor(waveform).unsqueeze(0)))},{speaker_m(torch.Tensor(waveform).unsqueeze(0))}')
+    output_speaker = speakers_list[np.argmax(speaker_m(torch.Tensor(waveform).unsqueeze(0)))]
+    return output_speaker
+
+
 @app.callback(
     [
         Output("gt_player", "src"),
@@ -136,7 +185,13 @@ def get_src_from_sound_file_path(data_sound_file):
         Output("base_speaker_index_selector", "options"),
         Output("base_speaker_selector", "options"),
         Output("target_speaker_selector", "options"),
-        Output("target_accent_selector", "options")
+        Output("target_accent_selector", "options"),
+        Output('Base_Accent_results', "children"),
+        Output('Base_speaker_results', "children"),
+        Output('gt_gen_Accent_results', "children"),
+        Output('gt_gen_speaker_results', "children"),
+        Output('new_gen_Accent_results', "children"),
+        Output('new_gen_speaker_results', "children"),
     ],
     [
         Input("anonymous_button", "value"),
@@ -147,6 +202,7 @@ def get_src_from_sound_file_path(data_sound_file):
     ],
 )
 def update_graphs(keep_anonymous, base_speaker, target_speaker, target_accent, base_speaker_index):
+    print(f'base_speaker:{base_speaker}, target_speaker:{target_speaker}, target_accent:{target_accent}')
     sound_file_output_path = inference_alon.generate_from_speakers(generator=GENERATOR,
                                                                    base_speaker=base_speaker,
                                                                    target_speaker=target_speaker,
@@ -166,7 +222,27 @@ def update_graphs(keep_anonymous, base_speaker, target_speaker, target_accent, b
     else:
         speaker_selector = [{"label": f'{r}_{speakers_data_dict[r]}', "value": r} for r in speakers_list]
 
-    return gt_src, gt_gen_src, new_src, utterance_options, speaker_selector, speaker_selector, speaker_selector
+    gt_accent = get_accent_classification(sound_file_output_path['gt'])
+    gt_gen_accent = get_accent_classification(sound_file_output_path['gt_gen'])
+    new_accent = get_accent_classification(sound_file_output_path['new'])
+
+    gt_speaker = get_speaker_classification(sound_file_output_path['gt'])
+    gt_gen_speaker = get_speaker_classification(sound_file_output_path['gt_gen'])
+    new_speaker = get_speaker_classification(sound_file_output_path['new'])
+
+    return gt_src,\
+           gt_gen_src,\
+           new_src,\
+           utterance_options,\
+           speaker_selector,\
+           speaker_selector,\
+           speaker_selector,\
+           gt_accent,\
+           gt_speaker, \
+           gt_gen_accent,\
+           gt_gen_speaker, \
+           new_accent,\
+           new_speaker
 
 
 if __name__ == "__main__":
